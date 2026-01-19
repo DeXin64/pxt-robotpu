5e548ee86d0ef89f3875d44dad136a8a85617404
uio64 <2692571183@qq.com>
2026-01-19 13:59:51 +0800

    fix: 调整控制延迟时间为5微秒以提升响应速度

--- main.ts
+++ main.ts
@@ -332,7 +332,7 @@ namespace robotPu {
                 }
                 
                 // 短暂延迟，控制更新频率
-                // control.waitMicros(50000); // 50ms
+                control.waitMicros(5); // 50ms
             }
         });
     }
